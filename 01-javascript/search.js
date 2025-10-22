1114e0a0d6a4b455e54bfaa830f66a153edac502
Aitor <aitordediego23@gmail.com>
2025-10-22 16:03:21 +0200

    Enhance search functionality by displaying results count and improving visibility handling

--- 01-javascript/search.js
+++ 01-javascript/search.js
@@ -1,34 +1,38 @@
 const searchInput = document.querySelector('#search-jobs')
 const jobsListingSection = document.querySelector('.jobs-listings')
 const jobsSection = document.querySelector('.jobs')
+const resultsFound = document.querySelector('#results-found')
 
-function displayResultsCount(found, total) {
-	let resultsFound = document.querySelector('#results-found')
-
-	if (!resultsFound) {
-		resultsFound = document.createElement('p')
-		resultsFound.id = 'results-found'
-		resultsFound.classList.add('mb-2')
-		jobsSection.insertBefore(resultsFound, jobsListingSection)
-	}
-
+function displayResultsFound(found, total) {
 	resultsFound.textContent = `Mostrando ${found.length} de ${total.length} ofertas`
+	resultsFound.classList.toggle('hidden', found.length === total.length)
 }
 
-function getResults(searchTerm) {
+function filterJobs(searchTerm) {
 	const jobsCards = document.querySelectorAll('.jobs-listings__job-listing-card')
 	const jobsCardsArray = Array.from(jobsCards)
+
+	if (searchTerm === '') {
+		jobsCardsArray.forEach((jobCard) => jobCard.classList.toggle('hidden', false))
+
+		resultsFound.classList.add('hidden')
+
+		return
+	}
+
 	const foundJobs = jobsCardsArray.filter((jobCard) => {
 		const jobTitle = jobCard.getElementsByTagName('h3')[0].textContent.toLowerCase()
 		const jobMatch = jobTitle.includes(searchTerm)
-		jobCard.style.display = jobMatch ? '' : 'none'
+
+		jobCard.classList.toggle('hidden', !jobMatch)
+
 		return jobMatch
 	})
-
-	displayResultsCount(foundJobs, jobsCardsArray)
+	displayResultsFound(foundJobs, jobsCardsArray)
 }
 
 searchInput.addEventListener('input', (event) => {
-	const searchTerm = event.target.value.toLowerCase()
-	getResults(searchTerm)
+	const searchTerm = event.target.value.toLowerCase().trim()
+
+	filterJobs(searchTerm)
 })
