8da60876c4fa89590dbc4503809334e97fbf6e16
Aitor <aitordediego23@gmail.com>
2025-11-05 12:02:06 +0000

    Remove commented-out job rendering code from fetchJobs function

--- 01-javascript/fetch-data.js
+++ 01-javascript/fetch-data.js
@@ -7,40 +7,6 @@ export const fetchJobs = fetch('./data.json')
 .then((jobs) => {
 	allJobs = jobs
 
-	// jobs.forEach((job) => {
-  //   const jobArticle = document.createElement('article')
-  //   const headerArticle = document.createElement('header')
-  //   const headerWrapperArticle = document.createElement('div')
-  //   const titleArticle = document.createElement('h3')
-  //   const metaWrapperArticle = document.createElement('div')
-  //   const metaCompanyArticle = document.createElement('small')
-  //   const metaSeparator = document.createElement('small')
-  //   const metaLocationArticle = document.createElement('small')
-  //   const buttonApply = document.createElement('button')
-  //   const descriptionArticleWrapper = document.createElement('div')
-  //   const descriptionArticle = document.createElement('p')
-
-  //   jobArticle.className = 'jobs-listings__job-listing-card'
-  //   jobArticle.dataset.technology = job.data.technology
-  //   jobArticle.dataset.locationType = job.data.location_type
-  //   jobArticle.dataset.experience = job.data.experience
-  //   titleArticle.textContent = job.title
-  //   metaWrapperArticle.className = 'jobs-listings__job-details'
-  //   metaCompanyArticle.textContent = job.company
-  //   metaSeparator.textContent = ' | '
-  //   metaLocationArticle.textContent = job.location
-  //   descriptionArticle.textContent = job.description
-  //   buttonApply.className = 'button-apply-job'
-  //   buttonApply.textContent = 'Aplicar'
-
-  //   metaWrapperArticle.append(metaCompanyArticle, metaSeparator, metaLocationArticle)
-  //   headerWrapperArticle.append(titleArticle, metaWrapperArticle)
-  //   headerArticle.append(headerWrapperArticle, buttonApply)
-  //   descriptionArticleWrapper.appendChild(descriptionArticle)
-  //   jobArticle.append(headerArticle, descriptionArticleWrapper)
-  //   jobsListingSection.appendChild(jobArticle)
-  // })
-
 	return jobs
 })
 .catch(error => {
