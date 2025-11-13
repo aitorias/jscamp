c636b0394cb8abeef83e65226e7f5d26c58c6245
Aitor <aitordediego23@gmail.com>
2025-11-13 12:54:53 +0000

    Add id prop to Button component for improved accessibility

--- 02-react/src/features/ui/shared/button.component.jsx
+++ 02-react/src/features/ui/shared/button.component.jsx
@@ -1,6 +1,6 @@
-export function Button({ type, title, className, onClick, disabled, children, ...rest }) {
+export function Button({ type, id, title, className, onClick, disabled, children, ...rest }) {
 	return (
-		<button type={type} title={title} className={className} onClick={onClick} {...rest} disabled={disabled}>
+		<button type={type} id={id} title={title} className={className} onClick={onClick} {...rest} disabled={disabled}>
 			{children}
 		</button>
 	)
