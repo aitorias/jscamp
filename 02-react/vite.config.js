6ee983ae725da9def9b1817713ccd86591c7fb6c
Aitor <aitordediego23@gmail.com>
2025-11-13 12:28:30 +0000

    Add jsconfig.json for TypeScript path mapping and update vite.config.js with alias configuration

--- 02-react/vite.config.js
+++ 02-react/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@data': '/src/data',
+			'@features': '/src/features',
+			'@shared': '/src/features/ui/shared',
+			'@ui': '/src/features/ui',
+			'@utils': '/src/utils'
+		},
+		extensions: [
+			'.js', '.json', '.jsx', '.ts', '.tsx'
+		]
+	}
 })
