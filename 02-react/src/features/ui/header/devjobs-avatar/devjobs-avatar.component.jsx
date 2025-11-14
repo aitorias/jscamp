f53e73dead890a5698bb825cf1e3173478a6ac4e
Aitor <aitordediego23@gmail.com>
2025-11-14 13:12:09 +0000

    Create devjobs-avatar component

---
+++ 02-react/src/features/ui/header/devjobs-avatar/devjobs-avatar.component.jsx
@@ -0,0 +1,21 @@
+import { Image } from '@features/ui/shared'
+import { Link } from '@features/ui/shared'
+import { createUrl } from '@utils/create-url'
+import styles from '@ui/header/devjobs-avatar/devjobs-avatar.module.css'
+
+export function DevJobsAvatar({ service = 'github', username = 'aAitorias', size }) {
+	const url = createUrl(service, username)
+
+	return (
+		<Link href={'#'} className={styles.avatarLink}>
+			<Image
+				src={url}
+				alt={`Avatar de ${username} en ${service}`}
+				width={size}
+				height={size}
+				className={styles.avatarImage}
+				style={{ height: size, width: size }}
+			/>
+		</ Link>
+	)
+}
