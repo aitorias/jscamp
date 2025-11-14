23c53c304bef653c5da9dfe1ea531a8fa4cff96b
Aitor <aitordediego23@gmail.com>
2025-11-14 13:11:19 +0000

    Fixed button component

--- 02-react/src/features/ui/shared/button/button.component.jsx
+++ 02-react/src/features/ui/shared/button/button.component.jsx
@@ -1,10 +1,18 @@
 import styles from '@ui/shared/button/button.module.css'
 
-export function Button({ type, id, title, variant, className = '', onClick, disabled, children, ...rest }) {
+export function Button({ type = 'button', id, title, variant, className = '', onClick, disabled, children, ...rest }) {
 	const variantClass = variant ? styles[variant] : ''
+	const buttonClassName = `${styles.button} ${variantClass} ${className}`.trim()
 
 	return (
-		<button type={type} id={id} title={title} className={`${styles.button} ${variantClass} ${className}`} onClick={onClick} {...rest} disabled={disabled}>
+		<button
+			type={type}
+			id={id}
+			title={title}
+			className={buttonClassName}
+			onClick={onClick}
+			{...rest}
+			disabled={disabled}>
 			{children}
 		</button>
 	)
