2281546ad64a3f2fb0769c2bdc565c56d2f828de
Aitor <aitordediego23@gmail.com>
2025-11-19 16:04:59 +0000

    Added new results-summary component

---
+++ 02-react/src/features/jobs-search/results-summary/results-summary.component.jsx
@@ -0,0 +1,18 @@
+import styles from '@features/jobs-search/results-summary/results-summary.module.css'
+
+export function ResultsSummary({ totalResults, textToFilter }) {
+	if (totalResults === 0) return null
+
+	const isPlural = totalResults > 1
+	const verb = isPlural ? 'Se encontraron' : 'Se encontró'
+	const noun = isPlural ? 'trabajos' : 'trabajo'
+
+	return (
+		<div className={styles.resultsSummary}>
+			<p>
+				{verb} <strong>{totalResults}</strong> {noun}
+				{textToFilter && ` para "${textToFilter}"`}
+			</p>
+		</div>
+	)
+}
