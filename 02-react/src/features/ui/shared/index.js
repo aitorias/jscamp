ff7d90faed6ed66b53aa95fe440e5626c987a5e8
Aitor <aitordediego23@gmail.com>
2025-11-19 10:50:08 +0000

    Added new select component

--- 02-react/src/features/ui/shared/index.js
+++ 02-react/src/features/ui/shared/index.js
@@ -1,4 +1,5 @@
 export { Button } from './button/button.component.jsx'
 export { Image } from './image.component.jsx'
 export { Link } from './link.component.jsx'
+export { Select } from './select/select.component.jsx'
 export { SVG } from './svg.component.jsx'
---
+++ 02-react/src/features/ui/shared/select/select.component.jsx
@@ -0,0 +1,24 @@
+import styles from '@ui/shared/select/select.module.css'
+
+export function Select({ name, id, options, defaultValue, onChange }) {
+	return (
+		<select name={name} id={id} className={styles.select} onChange={onChange}>
+			<option value=''>{defaultValue}</option>
+			{options?.map((option) => (
+				option.options ? (
+					<optgroup key={option.label} label={option.label}>
+						{option.options.map((option) => (
+							<option key={option.value} value={option.value}>
+								{option.label}
+							</option>
+						))}
+					</optgroup>
+				) : (
+					<option key={option.value} value={option.value}>
+						{option.label}
+					</option>
+				)
+			))}
+		</select>
+	)
+}
