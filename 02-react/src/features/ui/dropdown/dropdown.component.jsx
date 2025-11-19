91ea54bfc8b4ca4d38221f61172dacb7ceaf8653
Aitor <aitordediego23@gmail.com>
2025-11-19 16:04:41 +0000

    Added new dropdown component

---
+++ 02-react/src/features/ui/dropdown/dropdown.component.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+import styles from '@ui/dropdown/dropdown.module.css'
+
+export function Dropdown({ ariaLabelledBy, className = '', hidden = false, open, onClose = () => { }, children }) {
+	const ref = useRef(null);
+
+	useEffect(() => {
+		if (!open) return;
+
+		const handleClickOutside = (e) => {
+			if (ref.current && !ref.current.contains(e.target)) {
+				onClose?.();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [open]);
+
+	return (
+		<div ref={ref} aria-labelledby={`${ariaLabelledBy}`} className={`${styles.dropdown} ${hidden ? 'hidden' : ''} ${className}`}>
+			{children}
+		</div >
+	)
+}
