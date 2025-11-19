1b58af9efc577294fb9ad96995028cd45e50a68c
Aitor <aitordediego23@gmail.com>
2025-11-19 17:32:18 +0000

    Removed main component from project and adapt the main content by the page

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,10 +1,23 @@
-import { Header, Footer, Main } from '@features/ui'
+import { Header, Footer } from '@features/ui'
+import { Error404Page, HomePage, SearchPage } from '@pages'
 
 export default function App() {
+	const currentPath = window.location.pathname
+
+	let pageComponent = <Error404Page />
+
+	if (currentPath === '/') {
+		pageComponent = <HomePage />
+	} else if (currentPath === '/search') {
+		pageComponent = <SearchPage />
+	} else {
+		pageComponent = <Error404Page />
+	}
+
 	return (
 		<>
 			<Header />
-			<Main />
+			{pageComponent}
 			<Footer />
 		</>
 	)
--- 02-react/src/features/ui/index.js
+++ 02-react/src/features/ui/index.js
@@ -1,5 +1,6 @@
 export * from './header'
+export * from './home-cards'
 export * from './shared'
 export { Footer } from './footer/footer.component.jsx';
+export { Hero } from './hero/hero.component.jsx'
 export { JobsSection } from './jobs-section.component.jsx'
-export { Main } from './main.component.jsx';
