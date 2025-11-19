3d737de742248659b691d754477d73eb18ad4a54
Aitor <aitordediego23@gmail.com>
2025-11-19 19:33:25 +0000

    Move the navigation functionality to header-nav instead of link

--- 02-react/src/features/ui/shared/header/header-nav/header-nav.component.jsx
+++ 02-react/src/features/ui/shared/header/header-nav/header-nav.component.jsx
@@ -1,12 +1,24 @@
 import { Link } from '@features/ui/shared'
+import { useRouter } from "@hooks/react-hooks"
 import styles from './header-nav.module.css'
 
 export function HeaderNav({ links }) {
+	const { pathname, navigateTo } = useRouter()
+
+	const handleNavigation = (href) => (event) => {
+		event.preventDefault()
+
+		navigateTo(href)
+	};
+
 	return (
 		<nav className={styles.nav}>
-			{links.map((link) => (
-				<Link key={link.href} href={link.href}>{link.text}</Link>
-			))}
+			{links.map((link) => {
+				const isActive = pathname === link.href
+				const linkClassName = `${styles.link} ${isActive ? styles.active : ''}`
+
+				return <Link key={link.href} href={link.href} className={linkClassName} onClick={handleNavigation}>{link.text}</Link>
+			})}
 		</nav>
 	)
 }
