3a38eaf4130cc3aa48b05f96c1117ebf0109d62e
Aitor <aitordediego23@gmail.com>
2025-11-19 17:50:59 +0000

    Added root link for the logo

--- 02-react/src/features/ui/header/header.component.jsx
+++ 02-react/src/features/ui/header/header.component.jsx
@@ -1,6 +1,6 @@
 import { HeaderLinks } from '@features/ui/header/header-links/header-links.component.jsx'
 import { HeaderNav } from '@features/ui/header/header-nav/header-nav.component.jsx'
-import { SVG } from '@features/ui/shared'
+import { Link, SVG } from '@features/ui/shared'
 import styles from '@ui/header/header.module.css'
 
 export function Header() {
@@ -16,11 +16,13 @@ export function Header() {
 	return (
 		<header className={styles.header}>
 			<h1>
-				<SVG strokeWidth={2} viewBox={'0 0 24 24'}>
-					<polyline points="16 18 22 12 16 6"></polyline>
-					<polyline points="8 6 2 12 8 18"></polyline>
-				</SVG>
-				DevJobs
+				<Link href='/'>
+					<SVG strokeWidth={2} viewBox={'0 0 24 24'}>
+						<polyline points="16 18 22 12 16 6"></polyline>
+						<polyline points="8 6 2 12 8 18"></polyline>
+					</SVG>
+					DevJobs
+				</Link>
 			</h1>
 			<HeaderNav links={menuLinks} />
 			<HeaderLinks buttons={buttons} />
